273245365b5eabca8b51c3c770feeb245c3f258b
Vimal290704 <vimalyadavkr001@gmail.com>
2025-09-01 12:40:58 +0530

    prog: login and token creation completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const User = require("../models/userModel.js");
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 
 const signUpUser = async (req, res) => {
     const {username, email, password} = req.body;
@@ -39,4 +40,52 @@ const signUpUser = async (req, res) => {
         throw new Error("User data is not valid!");
     }
 };
-module.exports = signUpUser;
+
+function isEmailValid(email) {
+    return email.toLowerCase().includes('@');
+}
+
+const loginUser = async (req, res) => {
+    const {identifier, password} = req.body;
+    if (!identifier || !password) {
+        return res.status(400).send({error: "All fields are required"});
+    }
+
+    let user;
+    if (isEmailValid(identifier)) {
+        user = await User.findOne({email: identifier});
+    } else {
+        user = await User.findOne({username: identifier});
+    }
+
+    if (!user) {
+        return res.status(400).send({error: "User does not exist"});
+    }
+
+    try {
+        if (await bcrypt.compare(password, user.password)) {
+            const accessToken = await jwt.sign(
+                {userId: user.id, type: "access"},
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: "15m"}
+            );
+            const refreshToken = await jwt.sign(
+                {userId: user.id, type: "refresh"},
+                process.env.REFRESH_TOKEN_SECRET,
+                {expiresIn: "7d"}
+            );
+            return res.status(200).json({
+                accessToken: accessToken,
+                refreshToken: refreshToken
+            })
+        } else {
+            return res.status(401).send({error: "Password is incorrect"});
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(200).send({error: "Internal Server Error"});
+    }
+}
+
+
+module.exports = {signUpUser, loginUser};
--- backend/routers/userRoute.js
+++ backend/routers/userRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const signUpUser = require("../controllers/userController");
+const {signUpUser, loginUser} = require("../controllers/userController");
 
 router.post("/signup", signUpUser);
+
+router.post("/login", loginUser);
 
 module.exports = router;
